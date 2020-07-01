6a0d39e6d6566b0a42df52a646a1d3067198e354
SREEJITH MURALIDHARAN <sreejithmuralidharan@SREEJITHs-MacBook-Pro.local>
2020-07-01 10:03:19 +0100

    Added LoginrequiredMixin

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth import login, authenticate
 from django.views.generic import DetailView, UpdateView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Profile
 
 class ProfileDetailView(LoginRequiredMixin, DetailView):
